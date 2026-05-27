a03e349ec7d252b6f0ea3b5f2a42dfecd8fa2177
tejaswini041995 <tejaswinicloud10816@gmail.com>
2026-05-27 21:40:29 +0530

    implemented automatic ec2 deployment pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "web_sg" {
+
+  name = "docker-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_server" {
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = "devops-key"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              apt update -y
+              apt install docker.io -y
+
+              systemctl start docker
+              systemctl enable docker
+
+              docker pull harinath641/cloud-native-devops:v1
+
+              docker run -d -p 80:80 harinath641/cloud-native-devops:v1
+
+              EOF
+
+  tags = {
+    Name = "Cloud-Native-DevOps-Server"
+  }
+}
